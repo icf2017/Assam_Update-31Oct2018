f1a56df49785b38003e29badd0f0c31c1a50a9e1
Namam Sharma <naman.vdoit@gmail.com>
2018-10-31 17:00:56 +0530

    ICF Upload On 31/10/2018

---
+++ assam/model(15-12-2017)/model_version.rb
@@ -0,0 +1,1 @@
+def Model.last_modified_date() @last_modified_date ||= Time.utc(*[20, 57, 12, 25, 11, 2017, 6, 329, false, "IST"]); end
---
+++ assam/model/model_version.rb
@@ -0,0 +1,1 @@
+def Model.last_modified_date() @last_modified_date ||= Time.utc(*[37, 30, 12, 15, 12, 2017, 5, 349, false, "IST"]); end
